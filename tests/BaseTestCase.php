b6a972b26c0facc9c7ac25dd89dbc71dc18de252
ib92 <ibrahimhizeoui@gmail.com>
2017-08-01 11:38:09 +0200

    Adding files in tests maps and fixing the assertions test

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -1,5 +1,4 @@
 <?php
-
 declare(strict_types=1);
 
 namespace Billogram\Tests;
@@ -14,13 +13,13 @@ use PHPUnit\Framework\TestCase;
 /**
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
+
 abstract class BaseTestCase extends TestCase
 {
     /**
      * @return string|null the directory where cached responses are stored
      */
     abstract protected function getCacheDir();
-
     public static function setUpBeforeClass()
     {
         parent::setUpBeforeClass();
@@ -29,9 +28,7 @@ abstract class BaseTestCase extends TestCase
 
     /**
      * Get a real HTTP client. If a cache dir is set to a path it will use cached responses.
-     *
      * @param null $apiKey
-     *
      * @return CachedResponseClient|HttplugClient
      */
     protected function getHttpClient($apiKey = null)
@@ -42,7 +39,6 @@ abstract class BaseTestCase extends TestCase
             return new HttplugClient();
         }
     }
-
     /**
      * Get a mocked HTTP client that never do calls over the internet. Use this is you want to control the response data.
      *
@@ -55,7 +51,6 @@ abstract class BaseTestCase extends TestCase
     {
         $client = new MockedHttpClient();
         $client->addResponse(new Response($statusCode, [], $body));
-
         return $client;
     }
 }
--- tests/CachedResponseClient.php
+++ tests/CachedResponseClient.php
@@ -1,9 +1,7 @@
 <?php
 
-declare(strict_types=1);
-
+ declare(strict_types=1);
 namespace Billogram\Tests;
-
 use Http\Client\HttpClient;
 use Nyholm\Psr7\Factory\StreamFactory;
 use Nyholm\Psr7\Response;
@@ -14,6 +12,7 @@ use Psr\Http\Message\RequestInterface;
  *
  * @author Tobias Nyholm <tobias.nyholm@gmail.com>
  */
+
 class CachedResponseClient implements HttpClient
 {
     /**
@@ -28,7 +27,6 @@ class CachedResponseClient implements HttpClient
      * @var string
      */
     private $cacheDir;
-
     /**
      * @param HttpClient  $delegate
      * @param string      $cacheDir
@@ -40,7 +38,6 @@ class CachedResponseClient implements HttpClient
         $this->cacheDir = $cacheDir;
         $this->apiKey = $apiKey;
     }
-
     /**
      * {@inheritdoc}
      */
@@ -53,14 +50,12 @@ class CachedResponseClient implements HttpClient
         }
         $file = sprintf('%s/%s_%s', $this->cacheDir, $host, sha1($url));
         if (is_file($file) && is_readable($file)) {
-            $header = json_decode(file_get_contents($file.'headers.txt'), true);
-
+            $header = json_decode(file_get_contents($file."headers.txt"),true);
             return new Response(200, $header, (new StreamFactory())->createStream(unserialize(file_get_contents($file))));
         }
         $response = $this->delegate->sendRequest($request);
         file_put_contents($file, serialize($response->getBody()->getContents()));
-        file_put_contents($file.'headers.txt', json_encode($response->getHeaders()));
-
+        file_put_contents($file."headers.txt", json_encode($response->getHeaders()));
         return $response;
     }
 }
